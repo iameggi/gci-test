015bf77624683855f0c6ae68616b9f701d270810
muslimlife_eggifhr <eggifhr@gmail.com>
2025-10-08 00:44:40 +0700

    initial commit

---
+++ app/Models/Assignment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Assignment extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'course_id',
+        'title',
+        'description',
+        'deadline',
+    ];
+
+    protected $casts = [
+        'deadline' => 'datetime',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function submissions()
+    {
+        return $this->hasMany(Submission::class);
+    }
+}
